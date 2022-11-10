b5f9502222b4474c1cbba27cba5e2af338271654
ahucha <ahuchard91@gmail.com>
2022-11-10 16:45:58 +0100

    Offcanvas form add annonce

--- vue/vue_annonces/annonces.php
+++ vue/vue_annonces/annonces.php
@@ -1,3 +1,61 @@
 <?php
-echo "test";
 ?>
+
+
+<!-- Bouton/Offcanvas Form Ajouter  -->
+<button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">Ajouter une annonce</button>
+
+<div class="offcanvas offcanvas-end offcanvas-right" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel" style='width: 43vw;'>
+  <div class="offcanvas-header">
+    <h3  style='text-align:center; width:100%;'class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Ajouter une annonce</h3>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <form style='text-align:center; font-size:20px;'>
+        <div class='mb-3'>   
+            <label for="DateTrajet" class="form-label">Date du trajet</label>
+            <input type="datetime-local" class="form-control" id="DateTrajet">
+        </div>
+        <div style='display:flex; width:100%; justify-content:space-between; align-items: center;'>
+            <div class='mb-3' style="width:45%; display:flex; flex-direction:column;align-items:center;">   
+                <label for="DateTrajet" class="form-label">Lieu de départ</label>
+                <input type="text" class="form-control" id="DateTrajet">
+            </div>
+                <lord-icon
+                    src="https://cdn.lordicon.com/qeberlkz.json"
+                    trigger="loop-on-hover"
+                    colors="primary:#000000"
+                    state="hover"
+                    style="width:50px;height:50px">
+                </lord-icon>
+            <div class='mb-3' style="width:45%; display:flex; flex-direction:column;align-items:center;">     
+                <label for="DateTrajet" class="form-label">Lieu d'arrivée</label>
+                <input type="text" class="form-control" id="DateTrajet">
+            </div>
+        </div>
+            <div class='mb-3'>   
+            <label for="nbPlace" class="form-label">Nombre de places</label>
+            <select id='nbPlace' name='nbPlace'>
+                <?php
+                for($i=0;$i<=10;$i++)
+                {
+                    echo "<option value=".$i.">".$i."</option>";
+                }
+                ?>
+            </select>
+        </div>
+
+        <div class='mb-3'>
+            <label class="form-label">Fumeur autorisé ?</label>
+            <input type="radio" id="contactChoice1"
+            name="fumeur" value="1" >
+            <label for="contactChoice1">Oui</label>
+            <input type="radio" id="contactChoice2"
+            name="fumeur" value="0" checked>
+            <label for="contactChoice2">Non</label>
+        </div>
+        
+        <button type="submit" class="btn btn-dark">Ajouter</button>
+    </form>
+  </div>
+</div>
