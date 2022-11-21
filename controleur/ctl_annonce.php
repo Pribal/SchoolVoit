8a9332fdc327ff8921ed2606a3f475eb5e3dd866
ahucha <ahuchard91@gmail.com>
2022-11-21 09:39:18 +0100

    Add/Afficher Annonces

--- controleur/ctl_annonce.php
+++ controleur/ctl_annonce.php
@@ -6,7 +6,27 @@ $action =$_GET['action'];
 switch($action){	
 
     case 'vueAnnonces':
+        $iduser= $_SESSION["id"];
+        $listecar = DbAnnonce::getlistecar($iduser);
+        $listeAnnonce = DBAnnonce::getAnnonces($iduser);
         include('vue/vue_annonces/annonces.php');
+        break;
+
+    case 'ValidAjout':
+        $dateTrajet = $_POST['dateTrajet'];
+        $lieuDepart= $_POST['lieuDepart'];
+        $lieuArrivee= $_POST['lieuArrivee'];
+        $nbPlace= $_POST['nbPlace'];
+        $fumeur= $_POST['fumeur'];
+        $iduser= $_SESSION["id"];
+        $idcar= $_POST['idcar'];
+        $listecar = DbAnnonce::getlistecar($iduser);
+        DbAnnonce::ValidAjout($dateTrajet,$lieuDepart,$lieuArrivee,$nbPlace,$fumeur,$iduser,$idcar);
+        $listeAnnonce = DBAnnonce::getAnnonces($iduser);
+        include('vue/vue_annonces/annonces.php');
+        break;
+
+
 }
 
 ?>
